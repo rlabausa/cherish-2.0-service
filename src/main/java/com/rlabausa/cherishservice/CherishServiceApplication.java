381eb1d43999154b292742749d3e1cf9e4db446b
rlabausa <rubylabausa@gmail.com>
2024-04-24 17:26:28 -0600

    Scaffold Spring Boot application

---
+++ src/main/java/com/rlabausa/cherishservice/CherishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rlabausa.cherishservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherishServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherishServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlabausa/cherishservice/CherishServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlabausa.cherishservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherishServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
